0ec719b134b668157c5775c2eb9cf3b0a80a6f86
Dyllan Barreto <d.d.lan.db@gmail.com>
2023-04-22 12:32:55 -0300

    terceira aula
    lidando com promises pra evitar "callback hell"

--- service/cliente-service.js
+++ service/cliente-service.js
@@ -17,25 +17,23 @@
  const tabela = document.querySelector('[data-tabela]'); //resgatar o data-atributes pro javaScript saber onde mexer.
 
 
-
-const http = new XMLHttpRequest(); //XMLHttpRequest serve pra fazer a comunicação com a API.
-
-http.open('GET', 'http://localhost:3000/profile'); //abrir comunicação entre aplicação e API.
-
-http.send(); // enviar a nossa requisição.
-
-http.onload = () => { //indicar pro javaScript o que vou fazer com a resposta que o server enviou pra mim.
-    const data = JSON.parse(http.response); //coloquei na variavel pra caso algum dia eu precise modificar, eu modifique só em um lugar. "JSON.parse" serve pra transformar o texto que o HTTP manda em objeto JavaScript. 
-    data.forEach(elemento => { //pra fazer uma varredura de cada cliente dentro do "data".
-        tabela.appendChild(criaNovaLinha(elemento.nome,elemento.email)); //pra pegar cada dado referente ao cliente.
-    });
-
-    /*const http2 = new XMLHttpRequest();
+const listaClientes = () => {
+    const promise = new Promise((resolve, reject) => {
+        const http = new XMLHttpRequest(); //XMLHttpRequest serve pra fazer a comunicação com a API.
+        http.open('GET', 'http://localhost:3000/profile'); //abrir comunicação entre aplicação e API.
+        
+        http.onload = () => { //indicar pro javaScript o que vou fazer com a resposta que o server enviou pra mim.
+            if(http.status >= 400){
+                reject(JSON.parse(http.response));
+            }else {
+                resolve(JSON.parse(http.response));
+            }
+        /*const http2 = new XMLHttpRequest();
     http2.open('GET', 'http://localhost:3000/profile/semanaPassada');
     http2.onload = () => {
         ..
         const http3 = new XMLHttpRequest();
-        http3.open('GET', 'http://localhost:3000/profile/semanaRetrasada'); // Exercicio de exemplo de uma problematica que pode acontecer, encadiando uma sequencia de operações assincronas.
+        http3.open('GET', 'http://localhost:3000/profile/semanaRetrasada'); // Exercicio de exemplo de uma problematica que pode acontecer, encadiando uma sequencia de operações assincronas. Gerando um CallBack Hell
         http3.onload = () => {
             ..
         }
@@ -43,4 +41,15 @@ http.onload = () => { //indicar pro javaScript o que vou fazer com a resposta qu
     }
 
     http2.send();*/
-}
+    }
+    http.send(); // enviar a nossa requisição.
+    }); //criando uma promise pra evitar o CallBack Hell referenciado acima.
+    return promise;
+} 
+
+listaClientes()
+.then( data => {// quando a promesa for resolvida e nao rejeitada, then ( entao ) é pro javaScript mostrar o que está abaixo.
+        data.forEach(elemento => { //pra fazer uma varredura de cada cliente dentro do "data".
+            tabela.appendChild(criaNovaLinha(elemento.nome,elemento.email)); //pra pegar cada dado referente ao cliente.
+        });
+});
